5a83d23f2de7394d00e60b1aff43a9a9f4d45f87
shukurulloh0927 <shukurulloh0927@gmail.com>
2026-04-26 22:30:42 +0500

    task1  vazifa yuklandi

--- task1.py
+++ task1.py
@@ -1,12 +1,14 @@
-matn=input("Matn kiriting: ")
-unli=['a','e','i','u','o',"o'",'A','E','I','U','O',"O'"]
-sum_unli=0
-sum_undosh=0
-for i in matn:
-    if i.isalpha():
-        if i in unli:
-            sum_unli+=1
-        else:
-            sum_undosh+=1
-matn = {"unli":sum_unli,"undosh":sum_undosh}
-print(matn)        
+def count_vowels_and_consonants(text: str) -> dict:
+
+    unli=['a','e','i','u','o',"o'",'A','E','I','U','O',"O'"]
+    sum_unli=0
+    sum_undosh=0
+    for i in text:
+        if i.isalpha():
+            if i in unli:
+                sum_unli+=1
+            else:
+                sum_undosh+=1
+    matn = {"unli":sum_unli,"undosh":sum_undosh}
+    return matn
+print(count_vowels_and_consonants("Salom!"))
